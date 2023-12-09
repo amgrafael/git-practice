0c4857f718b3a6ba535aadf32887069827c994dd
amgrafael <amgrafael12@gmail.com>
2023-12-09 14:47:42 +0000

    Adding a file for shell commands

---
+++ sh_command.sh
@@ -0,0 +1,9 @@
+#This is the file for all the sh commands I have learned
+
+cd <Folder Name>
+ls -la <Looking for hidden files>
+ll <listing files>
+mkdir <creating a directory>
+cp <This is a copy command>
+
+
